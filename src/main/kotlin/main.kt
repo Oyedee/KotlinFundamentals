faae45c81f35b897b3a88f3aefd15a3da6aaf6d4
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2021-08-08 16:51:33 +0100

    how Kotlin improves the handling of Null values

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,17 +7,30 @@ fun main() {
 
     println("The answer to the question ${q.question} is ${q.answer}")
 
-    //using "if" as an expression
+    //using "if" as an expression and also assign the value of the expression result to a variable
     val message = if (q.answer == q.correctAnswer) {
         "you are correct"
     } else {
         "try Again"
     }
     println(message)
+
+    // Null can never be assigned to a non-null variable
+    //except we make the variable nullable by add a "?" to its declaration
+    //Handling null expression in kotlin
+    //How Kotlin improves the handling of null values
+
+    if(q.answer != null){}
+
+    // you can also make object "q" a nullable type by adding a "?" to the end object type
+    //change "q" to "p"
+    val p:Question? = Question()
 }
 
+
 class Question {
-    var answer: String = ""
+    //var answer: String = "" //non-nullable
+    var answer: String? = null //nullable
     val correctAnswer = "42"
     val question: String = "What is the answer to life, the universe and everything?"
 
